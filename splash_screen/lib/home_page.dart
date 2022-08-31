584a7c8a07c0bd1b3eb71064605e0b55ede3a17e
thomascwei <thomas.c.wei@gmail.com>
2022-08-31 14:11:56 +0800

    .

---
+++ splash_screen/lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('home'),
+      ),
+      body: Center(
+        child: Text('page'),
+      ),
+    );
+  }
+}
---
+++ splash_screen/lib/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'home_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 3000,
+      ),
+    );
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => MyHomePage()),
+            (route) => true);
+      }
+    });
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation,
+      child: Image.network(
+        'https://i.imgur.com/1XnxR43.jpeg',
+        scale: 2.0,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
